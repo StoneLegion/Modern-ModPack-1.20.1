5a4de3ffcf2f0cccf3f73531590a247c8d753822
StoneLegion <kanechart@gmail.com>
2024-11-19 15:38:55 -0500

    Added auto restart to run.sh

--- server/run.sh
+++ server/run.sh
@@ -1,6 +1,13 @@
-#!/usr/bin/env sh
-# Forge requires a configured set of both JVM and program arguments.
-# Add custom JVM arguments to the user_jvm_args.txt
-# Add custom program arguments {such as nogui} to this file in the next line before the "$@" or
-#  pass them to this script directly
+#!/bin/bash
+while true
+do
 /home/gtnh2041/jdk-17.0.11/bin/java @user_jvm_args.txt @libraries/net/minecraftforge/forge/1.20.1-47.3.12/unix_args.txt "$@"
+    echo "If you want to completely stop the server process now, press Ctrl+C before the time is up!"
+    echo "Rebooting in:"
+    for i in 12 11 10 9 8 7 6 5 4 3 2 1
+    do
+        echo "$i..."
+        sleep 1
+    done
+    echo "Rebooting now!"
+done
